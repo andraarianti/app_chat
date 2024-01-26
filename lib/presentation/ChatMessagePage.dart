3f3d0685cbe2cc6c96228fafa711d126b5f6546d
Andra Arianti <ariantiandra@gmail.com>
2024-01-26 11:19:06 +0700

    New Feature - List View (Show Username and Last Message) - Move to Chat Room Page (Show Message)

--- lib/presentation/ChatMessagePage.dart
+++ lib/presentation/ChatMessagePage.dart
@@ -4,6 +4,8 @@ import 'package:app_chat/domain/usecases/GetChatList.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../domain/usecases/GetChatUser.dart';
+
 class ChatMessagePage extends StatefulWidget{
   final String id;
   final String username;
@@ -34,38 +36,69 @@ class _ChatMessagePageState extends State<ChatMessagePage>{
       ),
       body: Column(
         children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                children: [
-                  Column(
+            Expanded(
+              child: Container(
+                  child: Column(
                     children: [
-                      // FutureBuilder<>(future: future, builder: builder)
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                          child: TextField(
-                            controller: _messageController,
-                            decoration: InputDecoration(
-                              hintText: 'Type a message ....'
-                            ),
-                          ),
+                      FutureBuilder<ChatList>(
+                        future: GetChatList().execute(widget.id),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            String errorMessage = snapshot.error?.toString() ?? 'Unknown error';
+                            return Center(child: Text('error on ChatListPage ${errorMessage}'));
+                          } else if (snapshot.hasData) {
+                            // Add a default return statement when snapshot.hasData is true but no specific widget to return.
+                            var data = snapshot.data!;
+                            var user = data.users;
+                            var message = data.messages;
+                            return Expanded(
+                              child: ListView.builder
+                                (
+                                  itemCount: message.length,
+                                  itemBuilder: (context, index){
+                                    var _listMessages = message[index].text;
+                                    var _listUsername = message[index].username;
+                                    return ListTile(
+                                      title: Text(_listUsername),
+                                      subtitle: Text(_listMessages),
+                                    );
+                                  }
+                                ),
+                            );
+                          } else {
+                            return Center(child: Text('Unknown Error'),);
+                          }
+                        },
                       ),
-                      IconButton(
-                          onPressed: (){
-
-                          },
-                          icon: Icon(Icons.send)
-                      )
                     ],
                   ),
+                ),
+            ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                          hintText: 'Type a message ....'
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.send)
+                  )
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
